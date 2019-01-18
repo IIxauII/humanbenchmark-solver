b2552be7493f179e64b5d079644a1e4a94fd8c42
DESKTOP-RDRMK5D\Felix H <failxtreme@icloud.com>
2019-01-18 18:56:11 +0100

    verbal memory test
    wrote js code to automatically solve verbal memory test

---
+++ verbal-memory.js
@@ -0,0 +1,36 @@
+startButton = document.querySelector('.button.start');
+if (startButton) {
+  startButton.click();
+} else {
+  console.log("No start button found. Are you on the correct page?\nhttps://www.humanbenchmark.com/tests/verbal-memory");
+}
+setTimeout(function(){
+  seenButton = document.querySelector('.actions').children[0];
+  newButton = document.querySelector('.actions').children[1];
+  wordArray = [];
+  setInterval(function () {
+    wordContainer = document.querySelector('.word.ng-scope > .ng-binding');
+    console.log('--');
+    console.log(wordArray);
+    console.log(wordContainer.textContent);
+    console.log('--');
+    if (wordArray.length) {
+      seen = false;
+      wordArray.forEach(function (word) {
+        if (word === wordContainer.textContent) {
+          seen = true;
+        }
+      });
+      if (seen) {
+        seenButton.click();
+      } else {
+        wordArray.push(wordContainer.textContent);
+        newButton.click();
+      }
+    } else {
+      wordArray.push(wordContainer.textContent);
+      newButton.click();
+    }
+  }, 50);
+}, 1000);
+
