a166d41183dc429ac40281bbaabc0236e6abf699
DESKTOP-RDRMK5D\Felix H <failxtreme@icloud.com>
2019-01-18 19:35:39 +0100

    updated verbal test

--- verbal-memory.js
+++ verbal-memory.js
@@ -1,10 +1,35 @@
-colorBox = document.querySelector('.reaction-time-test.view-splash');
-
-if (colorBox) {
-  
+startButton = document.querySelector('.button.start');
+if (startButton) {
+  startButton.click();
 } else {
-  while(document.firstChild) {
-    document.removeChild(document.firstChild);
-  }  
-  console.log("Are you on the correct page?\n Execute me here -> https://www.humanbenchmark.com/tests/reactiontime");
+  console.log("No start button found. Are you on the correct page?\nhttps://www.humanbenchmark.com/tests/verbal-memory");
 }
+setTimeout(function(){
+  seenButton = document.querySelector('.actions').children[0];
+  newButton = document.querySelector('.actions').children[1];
+  wordArray = [];
+  setInterval(function () {
+    wordContainer = document.querySelector('.word.ng-scope > .ng-binding');
+    console.log('--');
+    console.log(wordArray);
+    console.log(wordContainer.textContent);
+    console.log('--');
+    if (wordArray.length) {
+      seen = false;
+      wordArray.forEach(function (word) {
+        if (word === wordContainer.textContent) {
+          seen = true;
+        }
+      });
+      if (seen) {
+        seenButton.click();
+      } else {
+        wordArray.push(wordContainer.textContent);
+        newButton.click();
+      }
+    } else {
+      wordArray.push(wordContainer.textContent);
+      newButton.click();
+    }
+  }, 50);
+}, 1000);
